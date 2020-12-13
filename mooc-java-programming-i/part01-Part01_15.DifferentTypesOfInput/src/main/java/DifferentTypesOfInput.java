28e8b07733d8effadef32d6dfec4b52237c05beb
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-13 18:58:46 +0100

    Part01_14.TypesOfInput Learn reading different inputs

--- mooc-java-programming-i/part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
+++ mooc-java-programming-i/part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
@@ -7,6 +7,22 @@ public class DifferentTypesOfInput {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give a string:");
+        String word = scan.nextLine();
+        
+        System.out.println("Give an integer:");
+        int integer = Integer.valueOf(scan.nextLine());
+        
+        System.out.println("Give a double:");
+        double doubleNum = Double.valueOf(scan.nextLine());
+        
+        System.out.println("Give a boolean:");
+        boolean bool = Boolean.valueOf(scan.nextLine());
+        
+        System.out.println("You gave the string " + word);
+        System.out.println("You gave the integer " + integer);
+        System.out.println("You gave the double " + doubleNum);
+        System.out.println("You gave the boolean " + bool);
 
     }
 }
