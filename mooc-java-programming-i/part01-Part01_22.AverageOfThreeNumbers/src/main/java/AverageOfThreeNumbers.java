f7fc74a1e465804e95194f24a7bd096249d6fdbc
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-14 19:13:47 +0100

    Part01_20.AverageOfThreeNumbers Learn calculate with input and casting

--- mooc-java-programming-i/part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
+++ mooc-java-programming-i/part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
@@ -7,6 +7,16 @@ public class AverageOfThreeNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the third number:");
+        int third = Integer.valueOf(scanner.nextLine());
+        
+        double average = ((double) first + second + third) / 3;
+        System.out.println("The average is " + average);
     }
 }
