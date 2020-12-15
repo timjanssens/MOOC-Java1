ab9db9d25179f8a8169db203f9a21d85c6bdfcdd
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-15 19:32:48 +0100

    Part01_27.Ancient Learn If statements

--- mooc-java-programming-i/part01-Part01_27.Ancient/src/main/java/Ancient.java
+++ mooc-java-programming-i/part01-Part01_27.Ancient/src/main/java/Ancient.java
@@ -7,5 +7,13 @@ public class Ancient {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
+        int ancient = 2015;
+
+        System.out.println("Give a number");
+        int year = Integer.valueOf(scan.nextLine());
+
+        if (year < ancient) {
+            System.out.println("Ancient history!");
+        }
     }
 }
