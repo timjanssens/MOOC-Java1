c70a4db910ea510bbfeeaaa0ae2ca9c15ec5c4e3
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-15 19:31:27 +0100

    Part01_26.Orwel Learn If statements

--- mooc-java-programming-i/part01-Part01_26.Orwell/src/main/java/Orwell.java
+++ mooc-java-programming-i/part01-Part01_26.Orwell/src/main/java/Orwell.java
@@ -7,5 +7,14 @@ public class Orwell {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
+        
+        int orwel = 1984;
+        
+        System.out.println("Give a number");
+        int year = Integer.valueOf(scan.nextLine());
+        
+        if (year == orwel){
+            System.out.println("Orwell");
+        }
     }
 }
