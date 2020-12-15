4f2801993a208def6c38b8d29b394d4f34ebb0c4
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-15 19:26:56 +0100

    Part01_25.CheckYourIndention Learn indention

--- mooc-java-programming-i/part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
+++ mooc-java-programming-i/part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
@@ -7,13 +7,12 @@ public class SpeedingTicket {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here. 
-        
         System.out.println("Give speed");
         int speed = Integer.valueOf(scanner.nextLine());
-        
+
         if (speed > 120) {
             System.out.println("Speeding ticket!");
         }
-        
+
     }
 }
