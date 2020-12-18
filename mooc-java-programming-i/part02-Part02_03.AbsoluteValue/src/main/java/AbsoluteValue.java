8e162f7833e31aa2710955c35437c6f8f46daff5
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-18 11:25:16 +0100

    Part02_03.AbsoluteValue Learn sqrt function

--- mooc-java-programming-i/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ mooc-java-programming-i/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -5,6 +5,15 @@ public class AbsoluteValue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int number = Integer.valueOf(scanner.nextLine());
+        int output = number;
+        
+        if (number < 0) {
+            output = number * -1;
+        }
+        
+        System.out.println(output);
+        
     }
 }
