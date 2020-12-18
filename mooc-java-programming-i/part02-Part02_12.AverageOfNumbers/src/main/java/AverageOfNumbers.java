6362af707bc7bd69e72372274db7bd5528b06485
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-18 19:12:52 +0100

    Part02_12.AverageOfNumbers Learn While loops

--- mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,5 +6,23 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int times = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            sum += number;
+            times++;
+        }
+
+        double average = sum / (double) times;
+        System.out.print("Average of the numbers: " + average);
+
     }
 }
