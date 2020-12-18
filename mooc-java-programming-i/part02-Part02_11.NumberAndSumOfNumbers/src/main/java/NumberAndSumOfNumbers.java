7c7a949e89fa5040b53c6aa1790c59270be48e35
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-18 19:10:12 +0100

    Part02_11.NumberAndSumOfNumbers Learn While loops

--- mooc-java-programming-i/part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -6,5 +6,23 @@ public class NumberAndSumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int times = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            sum += number;
+            times++;
+        }
+
+        System.out.print("Number of numbers: " + times);
+        System.out.println("");
+        System.out.print("Sum of the numbers: " + sum);
     }
 }
