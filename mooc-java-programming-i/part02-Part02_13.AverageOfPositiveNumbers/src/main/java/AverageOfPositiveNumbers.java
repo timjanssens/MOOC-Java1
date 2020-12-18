f4fd3b10099e5acd6578b4badeaf8e545d688920
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-18 19:18:43 +0100

    Part02_13.AverageOfPositiveNumbers Learn While loops

--- mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,29 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int times = 0;
+
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            if (number > 0) {
+                sum += number;
+                times++;
+                continue;
+            }
+        }
+
+        if (times > 0) {
+            double average = sum / (double) times;
+            System.out.print("Average of the numbers: " + average);
+        } else {
+            System.out.println("Cannot calculate the average");
+        }
+
     }
 }
