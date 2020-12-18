7278956e948efee2fabcaf658df8ffa02fc3e7c7
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-18 18:58:38 +0100

    Part02_08.NumberOfNumbers Learn While loops

--- mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -6,5 +6,19 @@ public class NumberOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int times = 0;
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if(number == 0) {
+                break;
+            }
+            
+            times++;
+        }
+        
+        System.out.print("Number of numbers: " + times);
     }
 }
