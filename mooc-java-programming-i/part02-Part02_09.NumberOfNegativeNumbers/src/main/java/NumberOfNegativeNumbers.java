ef9ec8fdd5453bbaebfd21e85574c60bb284aa59
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-18 19:01:58 +0100

    Part02_09.NumberOfNegativeNumbers Learn While loops

--- mooc-java-programming-i/part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ mooc-java-programming-i/part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -6,5 +6,24 @@ public class NumberOfNegativeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int negative = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number < 0){
+                negative++;
+                continue;
+            }
+            
+            if (number == 0) {
+                break;
+            }
+
+        }
+
+        System.out.print("Number of negative numbers: " + negative);
+
     }
 }
