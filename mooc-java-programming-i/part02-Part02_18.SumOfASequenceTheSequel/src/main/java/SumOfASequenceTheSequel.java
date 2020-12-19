115a23c2cafce96f13b50d3108281b376d4f27a8
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-19 15:48:43 +0100

    Part02_18.SumOfASequenceTheSequel Learn Loops

--- mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -6,5 +6,18 @@ public class SumOfASequenceTheSequel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("First number?");
+        int numberOne = Integer.valueOf(scanner.nextLine());
+
+        System.out.print("Second number?");
+        int numberTwo = Integer.valueOf(scanner.nextLine());
+
+        int sum = 0;
+
+        for (int i = numberOne; i <= numberTwo; i++) {
+            sum += i;
+        }
+        
+        System.out.println("The sum is: " + sum);
     }
 }
