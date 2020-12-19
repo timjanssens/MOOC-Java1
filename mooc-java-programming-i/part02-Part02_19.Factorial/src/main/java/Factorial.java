84a79952e275d36c2740889e75e9be6a7b053df6
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-19 15:57:16 +0100

    Part02_19.Factorial Learn Loops

--- mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,16 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Give a number: ");
+        int number = Integer.valueOf(scanner.nextLine());
+        
+        int fact = 1;
+        
+        for (int i = 1; i <= number; i++) {
+            fact *= i;
+        }
+        
+        System.out.println("Factorial: " + fact);
+        
     }
 }
