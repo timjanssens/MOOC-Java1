15bc30353a45f0333651333dde8ea3e49a937e6d
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-20 10:11:35 +0100

    Part02_23.Division Learn methods

--- mooc-java-programming-i/part02-Part02_25.Division/src/main/java/Division.java
+++ mooc-java-programming-i/part02-Part02_25.Division/src/main/java/Division.java
@@ -1,15 +1,18 @@
 
-
 public class Division {
 
     public static void main(String[] args) {
-        
+
         // Once you have implemented the division method, you can
         // try it out here. Fopr example division(3,5);
         // should print "0.6"
-
-        // division(3, 5);
+        division(3, 5);
     }
 
+    public static void division(int number1, int number2) {
+        double result = (double)number1 / number2;
+        
+        System.out.println(result);
+    }
     // implement the method here
 }
