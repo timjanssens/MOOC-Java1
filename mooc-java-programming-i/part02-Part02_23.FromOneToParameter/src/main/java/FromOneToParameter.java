9a6ccd854dc0993640f3f746e8eb7db989023988
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-20 10:00:33 +0100

    Part02_23.FromOneToParameter Learn methods

--- mooc-java-programming-i/part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ mooc-java-programming-i/part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -3,7 +3,13 @@
 public class FromOneToParameter {
 
     public static void main(String[] args) {
-
+        printUntilNumber(5);
     }
 
+    public static void printUntilNumber(int times){
+        int number = 1;
+        for (int i = 0; i < times; i++) {
+            System.out.println(number++);
+        }
+    }
 }
