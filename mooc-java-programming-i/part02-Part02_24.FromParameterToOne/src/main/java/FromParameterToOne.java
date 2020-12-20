f2244bebea8fa8b048164419eb1aee803c205bc7
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-20 10:05:56 +0100

    Part02_23.printFromNumberToOne Learn methods

--- mooc-java-programming-i/part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
+++ mooc-java-programming-i/part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -1,10 +1,14 @@
 
-
 public class FromParameterToOne {
 
     public static void main(String[] args) {
-
-
+        printFromNumberToOne(5);
     }
 
+    public static void printFromNumberToOne(int times) {
+
+        while (times > 0) {
+            System.out.println(times--);
+        }
+    }
 }
