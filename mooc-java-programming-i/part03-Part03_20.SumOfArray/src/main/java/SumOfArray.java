6c25ed5756f18d19859e900b6138fab7557373be
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-25 19:30:18 +0100

    Part03_20.SumOfArray Learn array in method is reference

--- mooc-java-programming-i/part03-Part03_20.SumOfArray/src/main/java/SumOfArray.java
+++ mooc-java-programming-i/part03-Part03_20.SumOfArray/src/main/java/SumOfArray.java
@@ -9,6 +9,12 @@ public class SumOfArray {
 
     public static int sumOfNumbersInArray(int[] array) {
         // Write some code here
-        return 0;
+        int sum = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        
+        return sum;
     }
 }
