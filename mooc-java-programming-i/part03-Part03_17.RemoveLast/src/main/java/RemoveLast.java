301f2e7bf4c682e519d538fafa637b0f31ce8836
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-25 14:20:56 +0100

    Part03_17.RemoveLast

--- mooc-java-programming-i/part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
+++ mooc-java-programming-i/part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
@@ -5,6 +5,31 @@ public class RemoveLast {
 
     public static void main(String[] args) {
         // Try your method in here
+
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add("First");
+        strings.add("Second");
+        strings.add("Third");
+
+        System.out.println(strings);
+
+        removeLast(strings);
+        removeLast(strings);
+
+        System.out.println(strings);
+    }
+
+    public static void removeLast(ArrayList<String> strings) {
+        
+        if(strings.size() == 0){
+            return;
+        }
+        
+        int indexToRemove = strings.size() - 1;
+        
+        strings.remove(indexToRemove);
+                        
     }
 
 }
