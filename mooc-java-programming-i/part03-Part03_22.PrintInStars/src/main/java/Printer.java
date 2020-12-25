4897ab4f72f13fdccc3793c6386ba2a611baef97
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-25 19:42:07 +0100

    Part03_20.Printer Learn array in method is reference

--- mooc-java-programming-i/part03-Part03_22.PrintInStars/src/main/java/Printer.java
+++ mooc-java-programming-i/part03-Part03_22.PrintInStars/src/main/java/Printer.java
@@ -9,6 +9,12 @@ public class Printer {
 
     public static void printArrayInStars(int[] array) {
         // Write some code in here
+        for (int i = 0; i < array.length; i++) {
+            for (int stars = 0; stars < array[i]; stars++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 
 }
