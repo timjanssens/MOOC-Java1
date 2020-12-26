026d5cae297edffd93c58c1e2faa188004d5f293
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-26 13:34:25 +0100

    Part03_25.Login Learn strings comparisson

--- mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
+++ mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
@@ -6,5 +6,27 @@ public class Login {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String username1 = "alex";
+        String password1 = "sunshine";
+        String username2 = "emma";
+        String password2 = "haskell";
+        
+        System.out.print("Enter username: ");
+        String guesName = scanner.nextLine();
+        System.out.print("Enter: password: ");
+        String guesPassword = scanner.nextLine();
+        
+        if (guesName.contains(username1) && guesPassword.contains(password1) ||
+                guesName.contains(username2) && guesPassword.contains(password2)) {
+            System.out.println("You have successfully logged in!");
+        } else{
+            System.out.println("Incorrect username or password!");
+        }
+        
+        
+        
+        
+        
+        
     }
 }
