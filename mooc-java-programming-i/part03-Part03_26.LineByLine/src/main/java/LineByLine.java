cd13fcf55b206d4eb0662d84da09d3c733092ae2
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-26 13:47:52 +0100

    Part03_26.LineByLine Learn strings comparisson check no entry

--- mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,7 +5,20 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")){
+                return;
+            }
+            
+            String[] words = input.split(" ");
+            
+            for(String word : words){
+                System.out.println(word);
+            }
+          
+        }
     }
 }
