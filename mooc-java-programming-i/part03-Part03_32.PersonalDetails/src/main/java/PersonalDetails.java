39690599a3c1b82cb594b5154e627631a48e7a2b
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-26 18:03:30 +0100

    Part03_32.PersonalDetails Learn strings comparisson check no entry

--- mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,29 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int totalYears = 0;
+        String nameLongest = "";
+        double person = 0;
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] words = input.split(",");
+
+            if (nameLongest.length() < words[0].length()) {
+                nameLongest = words[0];
+            }
+            
+            totalYears += Integer.valueOf(words[1]);
+            person++;
+        }
+
+        System.out.println("Longest name: " + nameLongest);
+        System.out.println("Average of the birth years: " + totalYears / person );
 
     }
 }
