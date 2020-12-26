b5d1abf7accb6a15a83c47c8f2423d95bb09151b
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-26 17:45:28 +0100

    Part03_30.AgeOfTheOldest Learn strings comparisson check no entry

--- mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -6,6 +6,24 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int ageOfOldest = 0;
 
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] words = input.split(",");
+
+            if (ageOfOldest <= Integer.valueOf(words[1])) {
+                ageOfOldest = Integer.valueOf(words[1]);
+            }
+
+        }
+        
+        System.out.println("Age of the oldest: " + ageOfOldest);
     }
 }
