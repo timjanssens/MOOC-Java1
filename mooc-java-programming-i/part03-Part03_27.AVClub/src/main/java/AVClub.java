f01d2c073e50f198294b3d64e6a8e9ec18fb47d7
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-26 13:57:55 +0100

    Part03_27.AVClub Learn strings comparisson check no entry

--- mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -6,6 +6,24 @@ public class AVClub {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        
+         while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")){
+                return;
+            }
+            
+            String[] words = input.split(" ");
+            
+              for (String word : words){
+                if (word.contains("av")){
+                    System.out.println(word);
+                }
+            }
+          
+        }
+      
 
     }
 }
