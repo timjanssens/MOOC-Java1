571745b77fa32e6561c1638d71e5aa2da27b34f3
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-28 15:35:07 +0100

    Part04_29.RecordsFromAFile Learn reading csv from a file

--- mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,19 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        System.out.println("Name of the file: ");
+        String fileName = scanner.nextLine();
+        
+        try (Scanner fileReader = new Scanner(Paths.get(fileName))) {
+            while(fileReader.hasNextLine()){
+                String line = fileReader.nextLine();
+                
+                String[] parts = line.split(",");
+                System.out.println(parts[0] +  ", age: " + parts[1] + " years");
+            }
+        } catch (Exception e) {
+        }
+        
     }
 }
