6ccf98437d9870cdbc3521a8a7fdfa128ea1433c
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-28 08:18:11 +0100

    Part04_14.PersonalInformation Learn big class

--- mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -10,5 +10,31 @@ public class PersonalInformationCollection {
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+         
+            System.out.print("First name: ");
+            String firstName = scanner.nextLine();
+            
+            if (firstName.isEmpty()) {
+                break;
+            }
+            
+            System.out.print("Last name: ");
+            String  lastName = scanner.nextLine();
+            
+            
+            System.out.print("Identification number: ");
+            String  identificationNumber = scanner.nextLine();
+            
+            
+            infoCollection.add(new PersonalInformation(firstName, lastName, identificationNumber));
+            
+        }
+
+        System.out.println("");
+        
+        for(PersonalInformation person : infoCollection){
+            System.out.println(person.getFirstName() + " " + person.getLastName() );
+        }
     }
 }
