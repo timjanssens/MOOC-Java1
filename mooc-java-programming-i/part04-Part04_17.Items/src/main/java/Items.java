820eb01ad657d39ae55824c7e89ac443922bf866
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-28 07:53:53 +0100

    Part04_17.Items

--- mooc-java-programming-i/part04-Part04_17.Items/src/main/java/Items.java
+++ mooc-java-programming-i/part04-Part04_17.Items/src/main/java/Items.java
@@ -10,5 +10,22 @@ public class Items {
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while(true){
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            
+            if(name.equalsIgnoreCase("")){
+                break;
+            }
+            
+            Item item = new Item(name);
+            
+            items.add(item);
+            
+        }
+        
+        for(Item item : items){
+            System.out.println(item);
+        }
     }
 }
