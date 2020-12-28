97b69003435a9cbc9efc751e499f54c7c6f76f04
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-28 09:11:02 +0100

    Part04_15.TelevisionPrograms Learn big class

--- mooc-java-programming-i/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ mooc-java-programming-i/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -9,5 +10,30 @@ public class Main {
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+
+            if (name.isEmpty()) {
+                break;
+            }
+
+            System.out.print("Duration: ");
+            int duration = Integer.valueOf(scanner.nextLine());
+
+            programs.add(new TelevisionProgram(name, duration));
+
+        }
+        
+        System.out.print("Program's maximum duration? ");
+        int maxDuration = Integer.valueOf(scanner.nextLine());
+        
+        for(TelevisionProgram program : programs){
+            if (program.getDuration() <= maxDuration){
+                System.out.println(program);
+            }
+        }
+        
     }
 }
