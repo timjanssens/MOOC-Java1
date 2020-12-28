412655e5e47fdb1e6c209fa8562e8801192cff16
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-28 11:31:49 +0100

    Part04_22.Cubes Learn read input

--- mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,24 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
 
+        
+        while(true){
+            String word = scanner.nextLine();
+            
+            int number;
+            
+            if (word.equalsIgnoreCase("end")){
+                break;
+            } else {
+                number = Integer.valueOf(word);
+            }
+            
+            System.out.println(Math.round(Math.pow(number, 3)));
+         
+        }
+        
+  
     }
 }
