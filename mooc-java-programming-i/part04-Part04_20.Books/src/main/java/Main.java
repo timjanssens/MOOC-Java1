85467dd0db0be9521619cf158f74ac7e1143d493
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-28 09:32:57 +0100

    Part04_20.Books

--- mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
+++ mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
@@ -1,12 +1,46 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+
         // implement here the program that allows the user to enter 
         // book information and to examine them
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Book> books = new ArrayList();
+
+        while (true) {
+            System.out.print("Title: ");
+            String title = scanner.nextLine();
+
+            if (title.isEmpty()) {
+                break;
+            }
+
+            System.out.print("Pages: ");
+            int pages = Integer.valueOf(scanner.nextLine());
+
+            System.out.print("Publication year: ");
+            int publicationYear = Integer.valueOf(scanner.nextLine());
+
+            books.add(new Book(title, pages, publicationYear));
+
+        }
+
+        System.out.print("What information will be printed? ");
+        String printChoice = scanner.nextLine();
+
+        if (printChoice.equals("everything")) {
+            for (Book book : books) {
+                System.out.println(book);
+            }
+        } else if (printChoice.equalsIgnoreCase("name")){
+            for(Book book : books){
+                System.out.println(book.getTitle());
+            }
+        }
 
     }
 }
