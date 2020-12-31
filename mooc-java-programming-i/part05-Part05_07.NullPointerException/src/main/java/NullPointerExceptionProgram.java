5c33de585fdfb7f47fe1132101728b706c75b20c
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-31 17:03:30 +0100

    Part05_08.NullPointerException

--- mooc-java-programming-i/part05-Part05_07.NullPointerException/src/main/java/NullPointerExceptionProgram.java
+++ mooc-java-programming-i/part05-Part05_07.NullPointerException/src/main/java/NullPointerExceptionProgram.java
@@ -6,7 +6,11 @@ public class NullPointerExceptionProgram {
     public static void main(String[] args) {
         // Implement a program, or modify a program so, that it causes 
         // the NullPointerException -error
-
+        
+        
+        String tim = null;
+        System.out.println(tim.toString());
+        
         ArrayList<String> lines = new ArrayList<>();
         lines.add("Never has a man influenced physics so profoundly as Niels Bohr in the early 1900's");
         lines.add("Going back to this time period, little was known about atomic structure; Bohr set out");
