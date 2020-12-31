d37bab44e2adb8daa73c3bcb2c373788c5bc8622
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-12-31 17:55:47 +0100

    Part05_09.HealthStation Learn classes in classes

--- mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,28 @@
 
 public class HealthStation {
 
-
+    private int weigh;
+    
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
-        return -1;
+
+        this.weigh++;
+        return person.getWeight();
+        
     }
 
+    public void feed(Person person) {
+        
+        person.setWeight(person.getWeight()+1);
+        
+    }
+
+    
+    
+    
+    public int weighings(){
+        
+        return this.weigh;
+        
+    }
 }
