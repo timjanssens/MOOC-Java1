3f398b0bb3aea608c2d040d4039a426a3e189cb5
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-01-01 11:23:34 +0100

    Part05_17.Money Learn classes in classes

--- mooc-java-programming-i/part05-Part05_17.Money/src/main/java/Money.java
+++ mooc-java-programming-i/part05-Part05_17.Money/src/main/java/Money.java
@@ -1,4 +1,6 @@
 
+import java.math.BigDecimal;
+
 public class Money {
 
     private final int euros;
@@ -23,6 +25,10 @@ public class Money {
         return this.cents;
     }
 
+    public int totalCents() {
+        return this.euros * 100 + this.cents;
+    }
+
     public String toString() {
         String zero = "";
         if (this.cents < 10) {
@@ -32,4 +38,33 @@ public class Money {
         return this.euros + "." + zero + this.cents + "e";
     }
 
+    public Money plus(Money addition) {
+
+        // create a new Money object that has the correct worth
+        int newEuros = this.euros + addition.euros;
+        int newCents = this.cents + addition.cents;
+
+        Money newMoney = new Money(newEuros, newCents);
+
+        // return the new Money object
+        return newMoney;
+    }
+
+    public boolean lessThan(Money compared) {
+        return this.totalCents() < compared.totalCents();
+    }
+
+    public Money minus(Money decreaser) {
+        int cents = 0;
+        int euros = 0;
+        if (this.totalCents() > decreaser.totalCents()) {
+            int rest = this.totalCents() - decreaser.totalCents();
+            euros = (int) rest / 100;
+
+            cents = rest % 100;
+            
+        }
+        Money newMoney = new Money(euros, cents);
+        return newMoney;
+    }
 }
