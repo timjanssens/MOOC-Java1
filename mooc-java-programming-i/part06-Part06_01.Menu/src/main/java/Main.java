706f5633d9f6add67c4c1b5b629db9743573aaca
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-01-01 13:22:07 +0100

    Part06_01.Menu Learn list in classes

--- mooc-java-programming-i/part06-Part06_01.Menu/src/main/java/Main.java
+++ mooc-java-programming-i/part06-Part06_01.Menu/src/main/java/Main.java
@@ -7,9 +7,9 @@ public class Main {
         // You can delete the comments below
         
 
-//        exactum.addMeal("Smoked salmon, white wine and butter sauce with basil");
-//        exactum.addMeal("Seasonal green salad with apple-honey vinegarette");
-//        exactum.addMeal("Roasted lamb in a red wine sauce");
+        exactum.addMeal("Smoked salmon, white wine and butter sauce with basil");
+        exactum.addMeal("Seasonal green salad with apple-honey vinegarette");
+        exactum.addMeal("Roasted lamb in a red wine sauce");
         
         // When you have completed the method printMeals()
         // you can remove the comment below
--- mooc-java-programming-i/part06-Part06_01.Menu/src/main/java/Menu.java
+++ mooc-java-programming-i/part06-Part06_01.Menu/src/main/java/Menu.java
@@ -10,4 +10,21 @@ public class Menu {
     }
 
     // implement the required methods here
+    
+    public void addMeal(String meal){
+        if(!this.meals.contains(meal)){
+                this.meals.add(meal);
+        }
+    }
+    
+    public void printMeals(){
+        for(String meal: meals){
+            System.out.println(meal);
+        }
+    }
+    
+    public void clearMenu(){
+        meals.clear();
+    }
+            
 }
