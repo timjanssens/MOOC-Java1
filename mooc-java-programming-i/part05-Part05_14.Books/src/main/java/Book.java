7bcb405ce988b9298b50e698bb143af216f030e5
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-01-01 08:31:22 +0100

    Part05_14.Books Learn check if object is in a list

--- mooc-java-programming-i/part05-Part05_14.Books/src/main/java/Book.java
+++ mooc-java-programming-i/part05-Part05_14.Books/src/main/java/Book.java
@@ -17,4 +17,23 @@ public class Book {
         return publicationYear;
     }
 
+    @Override
+    public boolean equals(Object compared) {
+        // if the variables are located in the same position, they are equal
+        if (this == compared) {
+            return true;
+        }
+
+        // if the compared object is not of type Book, the objects are not equal
+        if (!(compared instanceof Book)) {
+            return false;
+        }
+
+        // convert the object to a Book object
+        Book comparedBook = (Book) compared;
+
+        // if the values of the object variables are equal, the objects are, too
+        return this.name.equals(comparedBook.name) && this.publicationYear == comparedBook.getPublicationYear();
+    }
+
 }
