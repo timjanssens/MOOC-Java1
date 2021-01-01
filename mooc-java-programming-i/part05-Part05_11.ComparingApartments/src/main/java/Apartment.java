fb1cd5e20000ce5f244c69f205166cabc6f5143f
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-01-01 07:11:42 +0100

    Part05_11.ComparingApartments Learn classes in classes

--- mooc-java-programming-i/part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ mooc-java-programming-i/part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -11,4 +11,43 @@ public class Apartment {
         this.princePerSquare = pricePerSquare;
     }
 
+    public int getSquares() {
+        return this.squares;
+    }
+
+    public int getPrincePerSquare() {
+        return this.princePerSquare;
+    }
+
+    public int getRooms() {
+        return this.rooms;
+    }
+
+    public boolean largerThan(Apartment compared) {
+        if (this.squares > compared.getSquares()) {
+            return true;
+        }
+        return false;
+    }
+
+    public int priceDifference(Apartment compared) {
+        int dif = 0;
+        dif = (this.squares * this.princePerSquare) - (compared.princePerSquare * compared.squares);
+        //get absolute value
+        dif = Math.abs(dif);
+        return dif;
+
+    }
+
+    public boolean moreExpensiveThan(Apartment compared) {
+        int dif = 0;
+        dif = (this.squares * this.princePerSquare) - (compared.princePerSquare * compared.squares);
+        
+        if (dif > 0){
+            return true;
+        }
+        
+        return false;
+    }
+
 }
