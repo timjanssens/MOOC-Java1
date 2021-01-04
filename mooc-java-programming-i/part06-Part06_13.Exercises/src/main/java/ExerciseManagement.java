66797bd7507054a4c03eddd513928e177b4eeff9
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-01-04 19:28:11 +0100

    Part06_13.ExercisesOnTesting Learn Testing

--- mooc-java-programming-i/part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
+++ mooc-java-programming-i/part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
@@ -1,4 +1,20 @@
 
+import java.util.ArrayList;
+
 public class ExerciseManagement {
+
+    private ArrayList<String> exercises;
+
+    public ExerciseManagement() {
+        this.exercises = new ArrayList<>();
+    }
+
+    public ArrayList<String> exerciseList() {
+        return new ArrayList<>();
+    }
+
+    public void add(String exercise) {
+        this.exercises.add(exercise);
+    }
 
 }
--- mooc-java-programming-i/part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ mooc-java-programming-i/part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,28 @@
 
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 public class ExerciseManagementTest {
 
+    @Test
+    public void exerciseListEmptyAtBeginning() {
+        ExerciseManagement management = new ExerciseManagement();
+        assertEquals(0, management.exerciseList().size());
+
+    }
+
+    @Test
+    public void addingExerciseGrowsListByOne() {
+        ExerciseManagement management = new ExerciseManagement();
+        management.add("Write a test");
+        assertEquals(1, management.exerciseList().size());
+    }
+
+    @Test
+    public void addedExerciseIsInList() {
+        ExerciseManagement management = new ExerciseManagement();
+        management.add("Write a test");
+        assertTrue(management.exerciseList().contains("Write a test"));
+    }
 }
