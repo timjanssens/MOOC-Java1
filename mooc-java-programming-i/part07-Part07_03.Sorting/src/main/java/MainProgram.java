28d8ba1168ae9558cad8e4e7ffe0ea03a3f222d7
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-01-07 18:46:44 +0100

    Part07_0..Sorting Learn algorithms

--- mooc-java-programming-i/part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ mooc-java-programming-i/part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -6,4 +6,14 @@ public class MainProgram {
         int[] array = {3, 1, 5, 99, 3, 12};
     }
 
+    public static int smallest(int[] numbers) {
+        int smallest = numbers[0];
+        for (int number : numbers) {
+            if (smallest > number) {
+                smallest = number;
+            }
+        }
+        return smallest;
+    }
+
 }
